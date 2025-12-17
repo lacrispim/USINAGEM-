448cde36ad474f9f21b8d951426300f84d14dcf9
Larissa Crispim <larissacrispim15@gmail.com>
2025-12-17 14:28:28 +0000

    O gráfico "Tempo Perdido por Motivo" está com letras brancas muito ruins

--- src/components/charts/loss-reason-pie-chart.tsx
+++ src/components/charts/loss-reason-pie-chart.tsx
@@ -21,11 +21,11 @@ interface LossReasonPieChartProps {
 }
 
 const CHART_COLORS = [
-  'hsl(var(--chart-1))',
   'hsl(var(--chart-2))',
   'hsl(var(--chart-3))',
   'hsl(var(--chart-4))',
   'hsl(var(--chart-5))',
+  'hsl(var(--chart-1))',
 ];
 
 export function LossReasonPieChart({ data, loading }: LossReasonPieChartProps) {
