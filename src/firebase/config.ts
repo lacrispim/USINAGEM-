43f0e2ae8ea59cac5aac1a57ccdb6515fe6bba05
Larissa Crispim <larissacrispim15@gmail.com>
2026-01-13 09:07:14 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -10,11 +10,11 @@
 // project settings.
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyAI1XkNMBqRNPt59plsuP3-MFskCwgibqQ",
+  authDomain: "coffee-spark-sample-app-bc73f.firebaseapp.com",
+  projectId: "coffee-spark-sample-app-bc73f",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  messagingSenderId: "198751668692",
+  appId: "1:198751668692:web:3fb688f93402472118ee41",
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
