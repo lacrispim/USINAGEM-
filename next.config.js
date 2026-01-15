6e267f4bb32109600bae740137ca4d39f2f0f64c
Larissa Crispim <larissacrispim15@gmail.com>
2026-01-15 13:00:08 +0000

    O que vc está fazendo de errado que o vercel não está fluindo? https://g

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: 'pravatar.cc',
         port: '',
         pathname: '/**',
       },
